ddd96f1a379726f876e08878efec32cd481605fb
imjamin <imjamin@qq.com>
2018-12-08 22:29:17 +0800

    自定义starter方式2

--- spring-boot-advance/springboot-starter-my-redis/src/main/java/com/exampleboot/App.java
+++ spring-boot-advance/springboot-starter-my-redis/src/main/java/com/exampleboot/App.java
@@ -7,7 +7,7 @@ import org.springframework.context.ConfigurableApplicationContext;
 import redis.clients.jedis.Jedis;
 
 @SpringBootApplication
-@EnableRedis
+//@EnableRedis
 public class App {
     public static void main(String[] args) {
         ConfigurableApplicationContext context = SpringApplication.run(App.class);

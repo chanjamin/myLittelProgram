cbb5e31e8730d7cbcebbe4697a00bc43969d6873
imjamin <imjamin@qq.com>
2018-12-08 20:26:32 +0800

    spring aop 得到joinPoint

--- spring-boot-advance/spring-aop/src/main/java/com/example/aop/LogAdvice.java
+++ spring-boot-advance/spring-aop/src/main/java/com/example/aop/LogAdvice.java
@@ -1,8 +1,12 @@
 package com.example.aop;
 
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.After;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
+
+import java.util.Arrays;
 
 @Aspect
 @Component
@@ -10,5 +14,9 @@ public class LogAdvice {
     @Before("execution(void com.example.aop.JoinPoint.pointCut(*))")
     public void logBefore(){
         System.out.println("--------------------------------logBefore");
+    }
+    @After("execution(void com.example.aop.JoinPoint.pointCut(*))")
+    public void logAfter(JoinPoint joinPoint){
+        System.out.println("----------------logAfter===========arg="+ Arrays.asList(joinPoint.getArgs()));
     }
 }
